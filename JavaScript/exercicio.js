b6eb51d20a2042eeaa13bf25d286c60ff4863995
sayurifranco <lsshiroma@gmail.com>
2023-05-23 14:47:23 +0100

    Exercícios JS

---
+++ JavaScript/exercicio.js
@@ -0,0 +1,36 @@
+// 1 
+var x;
+var y = undefined;
+var z = 0;
+
+// 2 
+console.log(typeof w);
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
+
+// 3 
+var x = 10;
+console.log(x + y);
+let y = 15;
+
+// 4
+console.log(0 <= 0);
+console.log(1 < 1);
+console.log(-2 > -22);
+console.log(-22 > 0); 
+
+// 5
+
+if (50 > 100) {
+    console.log("Falta um tanto");
+  } else {
+    if ("1" == "-1") {
+      console.log("Quase lá");    
+    }
+  
+    if (!null) {
+      console.log("Sucesso");
+    }
+  }
+
