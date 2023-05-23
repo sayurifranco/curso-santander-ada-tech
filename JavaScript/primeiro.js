836cbfd98f3ec1b248b41b06f660be96020b56b7
sayurifranco <lsshiroma@gmail.com>
2023-05-23 11:32:57 +0100

    exemplos de variáveis

--- JavaScript/primeiro.js
+++ JavaScript/primeiro.js
@@ -1,2 +1,5 @@
 console.log('Hello, World!')
 
+console.log('Bem-vindo ao curso de JavaScript')
+
+// Comentário
---
+++ JavaScript/variaveis.js
@@ -0,0 +1,40 @@
+// VARIÁVEIS:
+
+// são pedacinhos de memória que reserva para guardar alguma informação. Ex: Livros dentro de uma estante
+
+let idade = 26; // Criando a primeira variável
+
+console.log(idade);
+
+
+// TIPOS DE VARIÁVEIS:
+
+idade = 30;             // Number
+let numero = 30;        // Number
+let altura = 1.77;      // Number
+let nome = 'Leticia';   // String (texto)
+let estudando = true;   // Boleano (true or false)
+
+console.log(numero, typeof numero);
+console.log(altura, typeof altura);
+console.log(nome, typeof nome);
+console.log(estudando, typeof estudando);
+
+let outra;
+
+console.log(outra);
+
+let nula = null;
+
+console.log(nula);
+
+var variavel = 30; // Não utilize o var
+
+const constante = 100;
+
+// constante = 20; Não pode alterar a constante, dá erro
+
+
+
+
+
