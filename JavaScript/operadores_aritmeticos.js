41e37c268ea7bb5323e0bc00310df95769a1e55a
sayurifranco <lsshiroma@gmail.com>
2023-05-23 14:01:54 +0100

    Aula sobre operadores aritméticos

---
+++ JavaScript/operadores_aritmeticos.js
@@ -0,0 +1,42 @@
+// OPERADORES ARITMETICOS
+
+let primeiroNumero = 20;
+let segundoNumero = 2;
+
+// Adição:             +
+
+const soma = primeiroNumero + segundoNumero;
+
+console.log('Soma =', soma);
+
+// Subtração:          -
+
+console.log('Subtração =', primeiroNumero - segundoNumero);
+
+// Multiplicação:      *
+
+console.log('Multiplicação =', primeiroNumero * segundoNumero);
+
+// Divisão:            /
+
+console.log('Divisão =', primeiroNumero / segundoNumero);
+
+// Exponenciação:      **
+
+console.log('Exponenciação =', primeiroNumero ** segundoNumero);
+
+// Resto da divisão:    %
+
+console.log('Resto da divisão =', primeiroNumero % segundoNumero);
+
+// Precedência de operadores
+
+console.log((3 + 7) * 3);
+
+console.log('A média das notas é: ', (10 + 7 + 8) / 3);
+
+// Biblioteca Math
+
+console.log('O valor de pi é: ', Math.PI);
+console.log('A raiz quadrade de 16 é: ', Math.sqrt(16));
+
